be7baa20f833ad5df19d0d2644e6ff71e0ee6f63
Parth <parth.praful.parakhiya@gmail.com>
2025-02-17 12:15:46 -0500

    added new UI changes lab-6

--- myapp/views.py
+++ myapp/views.py
@@ -1,19 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
-from .models import Category, Product
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from .models import Category
 
-# Updated index view using index.html (inherits from base.html)
 def index(request):
-    cat_list = Category.objects.all().order_by('id')[:10]
+    cat_list = Category.objects.only('id', 'name').order_by('id')[:10]
     return render(request, 'myapp/index.html', {'cat_list': cat_list})
 
-# Updated about view using about.html template
 def about(request):
     return render(request, 'myapp/about.html')
 
-# Updated detail view using detail.html template
 def detail(request, cat_no):
     category = get_object_or_404(Category, id=cat_no)
-    products = Product.objects.filter(category=category)
-    context = {'category': category, 'products': products}
-    return render(request, 'myapp/detail.html', context)
+    products = category.products.all()
+
+    if not products:
+        messages.warning(request, "No products available in this category.")
+
+    return render(request, 'myapp/detail.html', {'category': category, 'products': products})
