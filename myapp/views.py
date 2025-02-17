f26f17325a84bae61721f9db3b46f67c8cb7146e
Parth <parth.praful.parakhiya@gmail.com>
2025-02-17 11:46:44 -0500

    add new changes lab-6

--- myapp/views.py
+++ myapp/views.py
@@ -2,23 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
+# Updated index view using index.html (inherits from base.html)
 def index(request):
     cat_list = Category.objects.all().order_by('id')[:10]
-    product_list = Product.objects.all().order_by('-price')[:5]
-    context = {
-        'categories': cat_list,
-        'products': product_list
-    }
-    return render(request, 'myapp/index.html', context)
+    return render(request, 'myapp/index.html', {'cat_list': cat_list})
 
+# Updated about view using about.html template
 def about(request):
-    return HttpResponse("This is an Online Store APP.")
+    return render(request, 'myapp/about.html')
 
+# Updated detail view using detail.html template
 def detail(request, cat_no):
     category = get_object_or_404(Category, id=cat_no)
     products = Product.objects.filter(category=category)
-    context = {
-        'category': category,
-        'products': products
-    }
+    context = {'category': category, 'products': products}
     return render(request, 'myapp/detail.html', context)
